3972d8fab285f088e711e08d06596888eb2e89ec
Omar <oser520@gmail.com>
2023-11-24 11:32:57 -0500

    Add logic to save magics to file.

--- src/bin/gen_magic.rs
+++ src/bin/gen_magic.rs
@@ -1,4 +1,7 @@
 use ruky::magics::{compute_bmagics, compute_rmagics, Magics};
+use std::env;
+use std::fs::File;
+use std::io::Write;
 use std::thread::spawn;
 
 fn main() {
@@ -23,6 +26,40 @@ fn main() {
 fn process_magics(bmagics: &impl Magics, rmagics: &impl Magics) {
     print_info(bmagics, false);
     print_info(rmagics, true);
+
+    if let Some(fname) = env::args_os().nth(1) {
+        match File::create(&fname) {
+            Err(_) => exit_with_err("Unable to open file for writing."),
+            Ok(mut file) => {
+                fwrite(
+                    &mut file,
+                    "BMAGICS",
+                    bmagics.as_ref().iter().map(|m| m.magic),
+                );
+                fwrite(
+                    &mut file,
+                    "RMAGICS",
+                    rmagics.as_ref().iter().map(|m| m.magic),
+                );
+            }
+        }
+    }
+}
+
+fn fwrite(ofile: &mut File, name: &str, magics: impl Iterator<Item = u64>) {
+    if let Err(_) = write!(ofile, "const {}: [u64: 64] = [\n", name) {
+        exit_with_err(format!("Unable to write magics for {}", name).as_ref());
+    }
+
+    for m in magics {
+        if let Err(_) = write!(ofile, "{:#x},\n", m) {
+            exit_with_err(format!("Unable to write magics for {}", name).as_ref());
+        }
+    }
+
+    if let Err(_) = write!(ofile, "];\n") {
+        exit_with_err(format!("Unable to write magics for {}", name).as_ref());
+    }
 }
 
 fn print_info(magics: &impl Magics, for_rook: bool) {

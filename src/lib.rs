276f783b91bcf8a63ae8a2be0de861c479b6e49c
Omar <oser520@gmail.com>
2024-01-28 22:34:55 -0500

    Remove unused function and remove pub from some modules that are meant to be used internally.

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,5 @@
 pub mod bitboard;
 pub mod magics;
-pub mod sq;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod piece;
+mod piece_set;
+mod sq;

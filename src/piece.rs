9436ca98fa0f86b510d21e192ce68be64460dde9
Omar <oser520@gmail.com>
2024-02-11 00:08:32 -0500

    Define helper functions is_king, is_queen, ... for Piece.

--- src/piece.rs
+++ src/piece.rs
@@ -8,6 +8,50 @@ pub enum Piece<T> {
     Pawn(T),
 }
 
+impl<T> Piece<T> {
+    pub fn is_king(&self) -> bool {
+        match *self {
+            Piece::King(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_queen(&self) -> bool {
+        match *self {
+            Piece::Queen(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_rook(&self) -> bool {
+        match *self {
+            Piece::Rook(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_bishop(&self) -> bool {
+        match *self {
+            Piece::Bishop(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_knight(&self) -> bool {
+        match *self {
+            Piece::Knight(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_pawn(&self) -> bool {
+        match *self {
+            Piece::Pawn(_) => true,
+            _ => false,
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum Color {
     White,

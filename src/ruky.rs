2947e50e5a0fba9a48702d08fff1f72b31f1ec34
Omar <oser520@gmail.com>
2024-03-02 22:11:29 -0500

    Create unit test moves_from_fen1().

--- src/ruky.rs
+++ src/ruky.rs
@@ -39,3 +39,87 @@ impl Ruky {
         self.from_fen(fen).map(|board| board.next_moves())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::piece::Piece::*;
+    use crate::piece_move::PieceMove::*;
+    use crate::sq;
+    use lazy_static::lazy_static;
+    use std::collections::HashSet;
+
+    // Initialize RUKY only once here, since it's a convenient wrapper around the
+    // magics.
+    lazy_static! {
+        static ref RUKY: Ruky = Ruky::new();
+    }
+
+    #[test]
+    fn moves_from_fen1() {
+        let moves = RUKY
+            .moves_from_fen("8/1P6/8/1K2n3/6Pp/8/5P2/2k5 w - - 0 1")
+            .expect("Fen is OK")
+            .expect("Moves are OK");
+
+        let expected_moves = HashSet::from([
+            Pawn(Promo {
+                from: sq::B7,
+                to: sq::B8,
+                promo: Knight(()),
+            }),
+            Pawn(Promo {
+                from: sq::B7,
+                to: sq::B8,
+                promo: Bishop(()),
+            }),
+            Pawn(Promo {
+                from: sq::B7,
+                to: sq::B8,
+                promo: Rook(()),
+            }),
+            Pawn(Promo {
+                from: sq::B7,
+                to: sq::B8,
+                promo: Queen(()),
+            }),
+            Pawn(Simple {
+                from: sq::F2,
+                to: sq::F3,
+            }),
+            Pawn(Simple {
+                from: sq::F2,
+                to: sq::F4,
+            }),
+            Pawn(Simple {
+                from: sq::G4,
+                to: sq::G5,
+            }),
+            King(Simple {
+                from: sq::B5,
+                to: sq::A4,
+            }),
+            King(Simple {
+                from: sq::B5,
+                to: sq::A5,
+            }),
+            King(Simple {
+                from: sq::B5,
+                to: sq::A6,
+            }),
+            King(Simple {
+                from: sq::B5,
+                to: sq::B4,
+            }),
+            King(Simple {
+                from: sq::B5,
+                to: sq::B6,
+            }),
+            King(Simple {
+                from: sq::B5,
+                to: sq::C5,
+            }),
+        ]);
+        assert_eq!(HashSet::from_iter(moves), expected_moves);
+    }
+}

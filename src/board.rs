fdb2474a86ba3628b1b06b195c5ea84c6411cdb6
Omar <oser520@gmail.com>
2024-03-03 19:37:47 -0500

    Add TODO to check validity of passant.

--- src/board.rs
+++ src/board.rs
@@ -722,6 +722,8 @@ impl BoardBuilder {
     }
 
     pub fn set_passant(&mut self, target: Sq) -> &mut Self {
+        // TODO: need to check that passant squares are valid, pawn is found at
+        // actual square.
         self.passant_sq = PassantSq::from_target(target);
         self
     }

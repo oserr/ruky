23799f2e8c2e6afcc3c6fc2f98e01f76c5699041
Omar <oser520@gmail.com>
2024-03-10 21:08:44 -0400

    Import some names.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 #![allow(dead_code)]
 pub mod bitboard;
 pub mod board;
-pub mod magics;
-pub mod ruky;
 mod fen;
+pub mod magics;
 mod piece;
 mod piece_move;
 mod piece_set;
+pub mod ruky;
 mod sq;
+
+pub use board::{Board, BoardBuilder};
+pub use piece_move::PieceMove;
+pub use ruky::Ruky;

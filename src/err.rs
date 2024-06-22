5d2b9bc94b8106629fc6e1f0f9b551f77a10f04e
Omar <oser520@gmail.com>
2024-06-22 16:58:38 -0400

    Pass in the name of the field and the number being converted for a more clear error message.

--- src/err.rs
+++ src/err.rs
@@ -1,7 +1,7 @@
 // En enum to represent all errors in the library.
 #[derive(Debug, Clone, PartialEq)]
 pub enum UziErr {
-    BadMillis,
+    BadMillis(String, String),
     BadNumber,
     GoErr,
     MissingCmd,

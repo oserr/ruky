c4e0681569d4d841013f9ae208547abbe06b72a4
Omar <oser520@gmail.com>
2024-06-22 12:48:37 -0400

    Add more UziErr errors used when parsing the Go command.

--- src/err.rs
+++ src/err.rs
@@ -1,6 +1,9 @@
 // En enum to represent all errors in the library.
 #[derive(Debug, Clone, PartialEq)]
 pub enum UziErr {
+    BadMillis,
+    BadNumber,
+    GoErr,
     MissingCmd,
     MissingOnOff,
     NothingSetForGo,

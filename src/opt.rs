a1593495084367692180926629b748dd311578aa
Omar <oser520@gmail.com>
2024-06-23 20:27:38 -0400

    Remove line breaks.

--- src/opt.rs
+++ src/opt.rs
@@ -6,43 +6,32 @@
 pub enum UciOpt {
     // The value in MB for memory for hash tables.
     Hash,
-
     // The path on the hard disk to the Nalimov compressed format. Multiple directories can be
     // concatenated with ";".
     NalimovPath(String),
-
     // This is the size in MB for the cache for the nalimov table bases.
     NalimovCache(OptVal),
-
     // This means that the engine is able to ponder.
     Ponder(bool),
-
     // This means that the engine has its own book which is accessed by the engine itself. If this
     // is set, the engine takes care of the opening book. If set to false, the engine should not
     // its book.
     OwnBook(bool),
-
     // The engine supports multi best line or k-best mode. The default value is 1.
     MultiPv(OptVal),
-
     // UCI_ShowCurrLine: The engine can show the current line it is calculating.
     ShowCurrLine(bool),
-
     // UCI_ShowRefutations: The engine can show a move and its refutations in a line.
     ShowRefutations(bool),
-
     // UCI_LimitStrength: The engine is able to limit its strength to a specific elo rating. This
     // should always be implemented together with "UCI_Elo".
     LimitStrength(bool),
-
     // UCI_Elo: The engine can limit its strengh in Elo within this interval. Should be
     // implemented together with UCI_LimitStrength.
     Elo(OptVal),
-
     // UCI_AnalsysMode: The engine wants to behave differently when analysing or playing a game.
     // This is set to false if the engine is playing a game.
     AnalysisMode(bool),
-
     // UCI_Opponent: The command can be used by the GUI to send the name, title, elo and if the
     // engine is playing a human or computer to the engine. The format of the string is:
     // - [GM|IM|FM|WGM|WIM|none] [<elo>|none] [computer|human] <name>, e.g.:
@@ -54,18 +43,14 @@ pub enum UciOpt {
         player_type: PlayerType,
         name: String,
     },
-
     // UCI_EngineAbout: The engine tells the GUI information about itself.
     About(String),
-
     // UCI_ShredderbasesPath: Path to folder of containing the Shredder endgame databases.
     ShredderBasesPath(String),
-
     // UCI_SetPositionValue: The GUI can send this to the engine to tell it to use a certain value
     // in centipawns from white's point of view if evaluating this specific position. Allowed
     // formats:
     SetPositionValue,
-
     // type <t>: The option has type <t>: check, spin, combo, button, string.
     Type(OptType),
 }

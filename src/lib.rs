9beae44adc0e9ac6bcdc11dd5ef107c6f11e9ab8
Omar <oser520@gmail.com>
2024-07-04 17:40:52 -0400

    Allow dead code at create level.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 // uzi for univeral zhess interface, a library that implements the UCI (Univeral
 // Chess Interface).
 
+// TODO: Try to remove this at crate level when the lib is more fleshed out.
+#![allow(dead_code)]
+
 mod conv;
 mod eng;
 mod err;

25219d6b4f0b42cd367b0fdf93a2a9ce6bea48ce
Omar <oser520@gmail.com>
2024-07-05 23:56:45 -0400

    Add unit tests for Piece conversion to and from str, char, and u8.

--- src/piece.rs
+++ src/piece.rs
@@ -19,7 +19,7 @@ impl Piece {
     pub fn as_str(&self) -> &'static str {
         match *self {
             Piece::King => "k",
-            Piece::Queen => "a",
+            Piece::Queen => "q",
             Piece::Rook => "r",
             Piece::Bishop => "b",
             Piece::Knight => "n",
@@ -30,7 +30,7 @@ impl Piece {
     pub fn to_u8(&self) -> u8 {
         match *self {
             Piece::King => b'k',
-            Piece::Queen => b'a',
+            Piece::Queen => b'q',
             Piece::Rook => b'r',
             Piece::Bishop => b'b',
             Piece::Knight => b'n',
@@ -41,7 +41,7 @@ impl Piece {
     pub fn to_char(&self) -> char {
         match *self {
             Piece::King => 'k',
-            Piece::Queen => 'a',
+            Piece::Queen => 'q',
             Piece::Rook => 'r',
             Piece::Bishop => 'b',
             Piece::Knight => 'n',
@@ -81,6 +81,7 @@ impl FromStr for Piece {
             "k" => Ok(Piece::King),
             "q" => Ok(Piece::Queen),
             "r" => Ok(Piece::Rook),
+            "b" => Ok(Piece::Bishop),
             "n" => Ok(Piece::Knight),
             "p" => Ok(Piece::Pawn),
             _ => Err(UziErr::ParsePieceErr(p.to_string())),
@@ -95,6 +96,7 @@ impl TryFrom<char> for Piece {
             'k' => Ok(Piece::King),
             'q' => Ok(Piece::Queen),
             'r' => Ok(Piece::Rook),
+            'b' => Ok(Piece::Bishop),
             'n' => Ok(Piece::Knight),
             'p' => Ok(Piece::Pawn),
             _ => Err(UziErr::ParsePieceErr(p.to_string())),
@@ -109,6 +111,7 @@ impl TryFrom<u8> for Piece {
             b'k' => Ok(Piece::King),
             b'q' => Ok(Piece::Queen),
             b'r' => Ok(Piece::Rook),
+            b'b' => Ok(Piece::Bishop),
             b'n' => Ok(Piece::Knight),
             b'p' => Ok(Piece::Pawn),
             _ => Err(UziErr::ParsePieceErr(p.to_string())),
@@ -119,5 +122,58 @@ impl TryFrom<u8> for Piece {
 impl Display for Piece {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> fmt::Result {
         formatter.write_str(self.as_str())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn piece_try_from_str() {
+        let str_to_piece: [(&str, Piece); 6] = [
+            ("k", Piece::King),
+            ("q", Piece::Queen),
+            ("r", Piece::Rook),
+            ("b", Piece::Bishop),
+            ("n", Piece::Knight),
+            ("p", Piece::Pawn),
+        ];
+        for (s, p) in &str_to_piece {
+            assert_eq!(Piece::from_str(*s), Ok(*p));
+            assert_eq!(p.as_str(), *s);
+        }
+    }
+
+    #[test]
+    fn piece_try_from_char() {
+        let char_to_piece: [(char, Piece); 6] = [
+            ('k', Piece::King),
+            ('q', Piece::Queen),
+            ('r', Piece::Rook),
+            ('b', Piece::Bishop),
+            ('n', Piece::Knight),
+            ('p', Piece::Pawn),
+        ];
+        for (c, p) in &char_to_piece {
+            assert_eq!(Piece::try_from(*c), Ok(*p));
+            assert_eq!(p.to_char(), *c);
+        }
+    }
+
+    #[test]
+    fn piece_try_from_u8() {
+        let byte_to_piece: [(u8, Piece); 6] = [
+            (b'k', Piece::King),
+            (b'q', Piece::Queen),
+            (b'r', Piece::Rook),
+            (b'b', Piece::Bishop),
+            (b'n', Piece::Knight),
+            (b'p', Piece::Pawn),
+        ];
+        for (b, p) in &byte_to_piece {
+            assert_eq!(Piece::try_from(*b), Ok(*p));
+            assert_eq!(p.to_u8(), *b);
+        }
     }
 }

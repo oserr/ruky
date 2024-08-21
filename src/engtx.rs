dd4c1b20ab5f43aadc32112461675a77a280000d
Omar <oser520@gmail.com>
2024-08-21 01:27:09 -0400

    Impl send_name and add some todos

--- src/engtx.rs
+++ src/engtx.rs
@@ -1,10 +1,11 @@
 // This module implements abstractions to transmit messages from the chess
 // engine to the GUI.
 
-use crate::engcmd::Info;
+use crate::engcmd::{EngCmd, Info};
 use crate::opt::HasOpt;
 use crate::pm::Pm;
 use std::sync::Arc;
+use tokio::io::{stdout, AsyncWriteExt};
 use tokio::runtime::Runtime;
 
 // A trait for tramitting commands from the communcation protocol engine to the
@@ -31,6 +32,46 @@ pub trait EngTx {
 #[derive(Clone, Debug)]
 struct UziEngOut {
     run_time: Arc<Runtime>,
+}
+
+impl EngOutTx for UziEngOut {
+    fn send_name(&self, name: String) {
+        self.run_time.spawn(async move {
+            let name_cmd = EngCmd::IdName(name);
+            let result = stdout().write(name_cmd.to_string().as_bytes()).await;
+            if let Err(_) = result {
+                todo!();
+            }
+        });
+    }
+
+    fn send_author(&self, _name: String) {
+        todo!();
+    }
+
+    fn send_uciok(&self) {
+        todo!();
+    }
+
+    fn send_ready(&self) {
+        todo!();
+    }
+
+    fn send_best(&self, _best: Pm) {
+        todo!();
+    }
+
+    fn send_ponder(&self, _best: Pm, _ponder: Pm) {
+        todo!();
+    }
+
+    fn send_info(&self, _info: Info) {
+        todo!();
+    }
+
+    fn send_opt(&self, _opt: HasOpt) {
+        todo!();
+    }
 }
 
 impl From<Arc<Runtime>> for UziEngOut {

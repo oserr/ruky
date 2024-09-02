bc2130036f1d6606ba62ac86a03b1bdf7b122bcc
Omar <oser520@gmail.com>
2024-09-02 13:15:41 -0400

    Begin implementing EngCon, the engine controller, with a high level outline of how it should handle the engine.

--- src/eng.rs
+++ src/eng.rs
@@ -1,10 +1,15 @@
 // This module defines the Eng trait, which is the main trait exposed by this
 // library for clients of the library to hook their engines with library.
 
+use crate::conf::Config;
+use crate::engtx::EngOutTx;
 use crate::err::UziErr;
-use crate::guicmd::{Go, Pos};
+use crate::guicmd::{Go, GuiCmd, Pos};
 use crate::opt::{Opponent, PosValueOpt};
+use std::io::stdin;
 use std::path::Path;
+use std::str::FromStr;
+use std::sync::Arc;
 
 pub trait Eng {
     fn set_hash_table_size(&mut self, table_size: u64) -> Result<(), UziErr>;
@@ -14,7 +19,6 @@ pub trait Eng {
     fn set_nalimov_cache(&mut self, cache_size: u64) -> Result<(), UziErr>;
 
     fn enable_ponder(&mut self) -> Result<(), UziErr>;
-
     fn disable_ponder(&mut self) -> Result<(), UziErr>;
 
     fn enable_own_book(&mut self) -> Result<(), UziErr>;
@@ -29,7 +33,6 @@ pub trait Eng {
     fn set_elo(&mut self, elo: u16) -> Result<(), UziErr>;
 
     fn enable_analysis(&mut self) -> Result<(), UziErr>;
-
     fn disable_analysis(&mut self) -> Result<(), UziErr>;
 
     fn set_shredder_bases_path<T: AsRef<Path>>(&mut self, path: T) -> Result<(), UziErr>;
@@ -45,4 +48,71 @@ pub trait Eng {
     fn stop(&mut self) -> Result<(), UziErr>;
 
     fn new_game(&mut self) -> Result<(), UziErr>;
+
+    fn quit(&mut self) -> Result<(), UziErr>;
+}
+
+// Represents the current engine state.
+// - Waiting - the engine is still waiting for the GUI to connect.
+// - Connected - the GUI is connected but has not started a new game.
+// - NewGame - The GUI has started a new game.
+// - Go - The engine is analyzing a position.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+enum EngState {
+    Waiting,
+    Connected,
+    NewGame,
+    Go,
+    Quit,
+}
+
+// The Uzi [Eng]ine [Con]troller.
+struct EngCon<E: Eng, O: EngOutTx> {
+    eng: E,
+    eng_out: Arc<O>,
+    conf: Config,
+    state: EngState,
+}
+
+impl<E: Eng, O: EngOutTx> EngCon<E, O> {
+    pub fn create(eng: E, eng_out: Arc<O>, conf: Config) -> Self {
+        Self {
+            eng: eng,
+            eng_out: eng_out,
+            conf: conf,
+            state: EngState::Waiting,
+        }
+    }
+
+    pub fn run(&mut self) -> Result<(), UziErr> {
+        for line in stdin().lines() {
+            match line {
+                // Here we should shut down gracefully and return an error.
+                Err(_err) => todo!(),
+                Ok(line) => {
+                    let cmd = GuiCmd::from_str(&line);
+                    match cmd {
+                        // TODO: log the error.
+                        Err(_) => continue,
+                        Ok(cmd) => self.handle_cmd(cmd),
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn handle_cmd(&mut self, cmd: GuiCmd) {
+        match cmd {
+            GuiCmd::Uci => todo!(),
+            GuiCmd::IsReady => todo!(),
+            GuiCmd::Debug(_is_enabled) => todo!(),
+            GuiCmd::SetOpt(_opt) => todo!(),
+            GuiCmd::Pos(_pos) => todo!(),
+            GuiCmd::NewGame => todo!(),
+            GuiCmd::Go(_go) => todo!(),
+            GuiCmd::Stop => todo!(),
+            GuiCmd::Ponderhit => todo!(),
+        }
+    }
 }

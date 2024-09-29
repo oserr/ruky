ec1c59d31842426c57566f1be673a3848c9f0139
Omar <oser520@gmail.com>
2024-09-29 00:26:38 -0400

    Impl SetOpt for the bool options.

--- src/eng.rs
+++ src/eng.rs
@@ -14,20 +14,21 @@ use std::str::FromStr;
 use std::sync::Arc;
 
 pub trait Eng {
-    fn set_hash_table_size(&mut self, table_size: u64) -> Result<(), UziErr>;
-    fn set_nalimov_path<T: AsRef<Path>>(&mut self, path: T) -> Result<(), UziErr>;
-    fn set_nalimov_cache(&mut self, cache_size: u64) -> Result<(), UziErr>;
-    fn set_ponder(&mut self, is_enabled: bool) -> Result<(), UziErr>;
-    fn set_own_book(&mut self, is_enabled: bool) -> Result<(), UziErr>;
-    fn set_multi_pv(&mut self, nlines: u64) -> Result<(), UziErr>;
+    fn hash_table_size(&mut self, table_size: u64) -> Result<(), UziErr>;
+    fn nalimov_path<T: AsRef<Path>>(&mut self, path: T) -> Result<(), UziErr>;
+    fn nalimov_cache(&mut self, cache_size: u64) -> Result<(), UziErr>;
+    fn ponder(&mut self, is_enabled: bool) -> Result<(), UziErr>;
+    fn own_book(&mut self, is_enabled: bool) -> Result<(), UziErr>;
+    fn multi_pv(&mut self, nlines: u64) -> Result<(), UziErr>;
     fn show_curr_line(&mut self, show_curr_line: bool) -> Result<(), UziErr>;
+    fn show_refutations(&mut self, show_refutations: bool) -> Result<(), UziErr>;
     fn limit_strength(&mut self, limit_strength: bool) -> Result<(), UziErr>;
-    fn set_elo(&mut self, elo: u16) -> Result<(), UziErr>;
-    fn set_analysis(&mut self, is_enabled: bool) -> Result<(), UziErr>;
-    fn set_shredder_bases_path<T: AsRef<Path>>(&mut self, path: T) -> Result<(), UziErr>;
-    fn set_opponent(&mut self, opponent: &Opponent) -> Result<(), UziErr>;
-    fn set_pos_val(&mut self, pos_val: &PosValueOpt) -> Result<(), UziErr>;
-    fn set_position(&mut self, pos: &Pos) -> Result<(), UziErr>;
+    fn elo(&mut self, elo: u16) -> Result<(), UziErr>;
+    fn analysis(&mut self, is_enabled: bool) -> Result<(), UziErr>;
+    fn shredder_bases_path<T: AsRef<Path>>(&mut self, path: T) -> Result<(), UziErr>;
+    fn opponent(&mut self, opponent: &Opponent) -> Result<(), UziErr>;
+    fn pos_val(&mut self, pos_val: &PosValueOpt) -> Result<(), UziErr>;
+    fn position(&mut self, pos: &Pos) -> Result<(), UziErr>;
     fn go(&mut self, go_cmd: &Go) -> Result<(), UziErr>;
     fn stop(&mut self) -> Result<(), UziErr>;
     fn new_game(&mut self) -> Result<(), UziErr>;
@@ -120,28 +121,63 @@ impl<E: Eng, O: EngOutTx> EngCon<E, O> {
     fn set_opt(&mut self, opt: SetOpt) {
         match opt {
             SetOpt::Hash(table_size) => set_spin_opt(table_size, self.conf.hash_table, |x| {
-                self.eng.set_hash_table_size(x)
+                self.eng.hash_table_size(x)
             }),
             SetOpt::NalimovPath(_path_buf) => todo!(),
             SetOpt::NalimovCache(cache_size) => {
                 set_spin_opt(cache_size, self.conf.nalimov_cache, |x| {
-                    self.eng.set_nalimov_cache(x)
+                    self.eng.nalimov_cache(x)
                 })
             }
-            SetOpt::Ponder(_enabled) => todo!(),
-            SetOpt::OwnBook(_enabled) => todo!(),
-            SetOpt::MultiPv(k_best) => {
-                set_spin_opt(k_best, self.conf.multi_pv, |x| self.eng.set_multi_pv(x))
+            SetOpt::Ponder(enabled) => {
+                set_bool_opt(enabled, self.conf.ponder.is_some(), |x| self.eng.ponder(x))
             }
-            SetOpt::ShowCurrLine(_enabled) => todo!(),
-            SetOpt::ShowRefutations(_enabled) => todo!(),
-            SetOpt::LimitStrength(_enabled) => todo!(),
-            SetOpt::Elo(elo) => set_spin_opt(elo, self.conf.elo, |x| self.eng.set_elo(x)),
-            SetOpt::AnalysisMode(_enabled) => todo!(),
+            SetOpt::OwnBook(enabled) => set_bool_opt(enabled, self.conf.own_book.is_some(), |x| {
+                self.eng.own_book(x)
+            }),
+            SetOpt::MultiPv(k_best) => {
+                set_spin_opt(k_best, self.conf.multi_pv, |x| self.eng.multi_pv(x))
+            }
+            SetOpt::ShowCurrLine(enabled) => {
+                set_bool_opt(enabled, self.conf.show_curr_line.is_some(), |x| {
+                    self.eng.show_curr_line(x)
+                })
+            }
+            SetOpt::ShowRefutations(enabled) => {
+                set_bool_opt(enabled, self.conf.show_refutations.is_some(), |x| {
+                    self.eng.show_refutations(x)
+                })
+            }
+            SetOpt::LimitStrength(enabled) => {
+                set_bool_opt(enabled, self.conf.limit_strength.is_some(), |x| {
+                    self.eng.limit_strength(x)
+                })
+            }
+            SetOpt::Elo(elo) => set_spin_opt(elo, self.conf.elo, |x| self.eng.elo(x)),
+            SetOpt::AnalysisMode(enabled) => {
+                set_bool_opt(enabled, self.conf.analysis_mode.is_some(), |x| {
+                    self.eng.analysis(x)
+                })
+            }
             SetOpt::ShredderBasesPath(_path_buf) => todo!(),
             SetOpt::Opp(_opponent) => todo!(),
             SetOpt::SetPosVal(_pos_val) => todo!(),
         }
+    }
+}
+
+fn set_bool_opt<F>(enabled: bool, is_supported: bool, mut setter_fn: F)
+where
+    F: FnMut(bool) -> Result<(), UziErr>,
+{
+    if !is_supported {
+        // TODO: log that feature is not supported.
+        return;
+    }
+
+    if let Err(_) = setter_fn(enabled) {
+        // TODO: Log some error here.
+        return;
     }
 }
 

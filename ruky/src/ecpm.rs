17bcda8cc9d2d629f21dffb78e10dbce989fff13
Omar <oser520@gmail.com>
2024-11-16 11:48:13 -0500

    Impl encode_under_promo to convert an under promo move to a code.

--- ruky/src/ecpm.rs
+++ ruky/src/ecpm.rs
@@ -1,5 +1,7 @@
 // This module contains logic to encode a piece move into a an encoded move.
 
+use crate::piece::Piece;
+
 // Represents the code for a given piece move, i.e, Piece<PieceMove>.
 struct EcMove {
     row: u8,
@@ -26,5 +28,24 @@ fn encode_knight_move(row_diff: i8, col_diff: i8) -> u8 {
             "row_diff={} and col_diff={} don't make sense for a knight move.",
             row_diff, col_diff
         ),
+    }
+}
+
+// Converts an underpromotion move, i.e. a promotion to a piece other than a
+// queen, to a number in the range [0, 8].
+fn encode_under_promo(col_diff: i8, promo: Piece<()>) -> u8 {
+    assert!(col_diff >= -1 && col_diff <= 1);
+
+    let code = match promo {
+        Piece::Rook(_) => 0,
+        Piece::Bishop(_) => 3,
+        Piece::Knight(_) => 6,
+        _ => panic!("piece={:?} is not valid for under promotion.", promo),
+    };
+
+    match col_diff {
+        0 => code + 1,
+        1 => code + 2,
+        _ => code,
     }
 }

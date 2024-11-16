b5016605fd47e4bdeae11fad366fd3f1f75602e3
Omar <oser520@gmail.com>
2024-11-15 19:24:20 -0500

    Define and impl the Policy head

--- znn/src/net.rs
+++ znn/src/net.rs
@@ -1,4 +1,5 @@
-// net is a module containing the building blocks of the neural network used in AlphaZero.
+// net is a module containing the building blocks of the neural network used in
+// AlphaZero.
 
 use burn::{
     nn::{
@@ -13,8 +14,8 @@ use burn::{
 // Residual Block
 //---------------
 
-// ResBlockNset implements the residual block in the AlphaZero network, which has 19 of these
-// blocks connected together.
+// ResBlockNset implements the residual block in the AlphaZero network, which
+// has 19 of these blocks connected together.
 struct ResBlockNet<B: Backend> {
     conv1: Conv2d<B>,
     conv2: Conv2d<B>,
@@ -49,5 +50,36 @@ impl<B: Backend> ResBlockNet<B> {
         let x = self.batch_norm2.forward(x);
         let x = x.clone() + x;
         relu(x)
+    }
+}
+
+//----------------
+// Policy head net
+//----------------
+
+struct PolicyNet<B: Backend> {
+    conv1: Conv2d<B>,
+    conv2: Conv2d<B>,
+    batch_norm: BatchNorm<B, 2>,
+}
+
+impl<B: Backend> PolicyNet<B> {
+    pub fn new(device: &Device<B>) -> Self {
+        Self {
+            conv1: Conv2dConfig::new([256, 256], [3, 3])
+                .with_padding(PaddingConfig2d::Same)
+                .init(device),
+            conv2: Conv2dConfig::new([256, 73], [3, 3])
+                .with_padding(PaddingConfig2d::Same)
+                .init(device),
+            batch_norm: BatchNormConfig::new(256).init(device),
+        }
+    }
+
+    pub fn forward(&self, x: Tensor<B, 4>) -> Tensor<B, 4> {
+        let x = self.conv1.forward(x);
+        let x = self.batch_norm.forward(x);
+        let x = relu(x);
+        self.conv2.forward(x)
     }
 }

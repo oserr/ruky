fa0f4f4b87bf2ad0271398409a782c98cd3f1ad6
Omar <oser520@gmail.com>
2024-11-16 12:16:50 -0500

    Impl encode_queen_move to encode a queen move to a number in range [0, 55]

--- ruky/src/ecpm.rs
+++ ruky/src/ecpm.rs
@@ -49,3 +49,33 @@ fn encode_under_promo(col_diff: i8, promo: Piece<()>) -> u8 {
         _ => code,
     }
 }
+
+// Converts a queen move to a number in the range [0, 55].
+fn encode_queen_move(row_diff: i8, col_diff: i8) -> u8 {
+    assert!(row_diff >= -7 && row_diff <= 7);
+    assert!(col_diff >= -7 && col_diff <= 7);
+    assert!(col_diff != 0 || col_diff != 0);
+
+    if row_diff < 0 {
+        let nrows = (row_diff * -1) as u8;
+        if col_diff == 0 {
+            nrows
+        } else if col_diff < 0 {
+            nrows + 7
+        } else {
+            nrows + 14
+        }
+    } else if row_diff == 0 {
+        if col_diff < 0 {
+            21 + (col_diff * -1) as u8
+        } else {
+            28 + col_diff as u8
+        }
+    } else if col_diff == 0 {
+        35 + row_diff as u8
+    } else if col_diff < 0 {
+        42 + row_diff as u8
+    } else {
+        49 + row_diff as u8
+    }
+}

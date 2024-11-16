70c476711d479ba7551382ea0c73ba679f9eb93e
Omar <oser520@gmail.com>
2024-11-15 21:11:43 -0500

    Add a scratch pad to test stuff.

---
+++ znn/src/bin/scratch.rs
@@ -0,0 +1,15 @@
+// This is simply a scratch pad to combine and tests stuff, either to verify
+// that it works, or simply to figure out how something works.
+
+use burn::backend::candle::{Candle, CandleDevice};
+use burn::tensor::Tensor;
+use znn::net::AlphaZeroNet;
+
+fn main() {
+    let device = CandleDevice::cuda(0);
+    let t = Tensor::zeros([1, 119, 8, 8], &device);
+    let alpha_zero = AlphaZeroNet::<Candle>::new(&device);
+    let (pol, val) = alpha_zero.forward(t);
+    println!("pol={}", pol);
+    println!("val={}", val);
+}

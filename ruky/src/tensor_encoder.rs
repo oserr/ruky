7c8c9144faa872a9d922dc028359c688eb1e79fb
Omar <oser520@gmail.com>
2024-11-16 21:24:40 -0500

    Add a comment to describe how board state is encoded

--- ruky/src/tensor_encoder.rs
+++ ruky/src/tensor_encoder.rs
@@ -1,5 +1,4 @@
-// This module contains components for encoding boards and moves to encoded
-// tensors.
+// This module contains components for encoding boards and moves to tensors.
 
 use crate::board::Board;
 use crate::search::{Bp, Mp};
@@ -11,3 +10,20 @@ trait TensorEncoder<B: Backend> {
     fn encode_mps(mps: &[Mp]) -> Tensor<B, 4>;
     fn encode_bps(bps: &[Bp]) -> Tensor<B, 4>;
 }
+
+// AzEncoder represents the AlphaZero encoder, i.e. it encodes the board the
+// board state for input into the AlphaZero network. For a given board position,
+// it outputs a 119 x 8 x 8 tensor. 6 planes are used to represent one set of
+// the pieces, 6 for the other set of pieces, and 2 planes for repetition count
+// in current position for each player. Hence, the current board position uses
+// 14 total planes, but this is repeated for 8 total time steps. If there are no
+// previous positions, the planes are set to zeros. The last 7 planes are used
+// to represent
+// - 1 for the current color
+// - 1 for the total move count
+// - 1 for king castling for the current player
+// - 1 for queenside castling for the current player
+// - 1 for king castling for the other player
+// - 1 for queen castling for the other player
+// - 1 for the progress count (i.e. 50 move rule)
+struct AzEncoder;

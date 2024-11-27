457b15385d19414b613620484a0581dbd1d49251
Omar <oser520@gmail.com>
2024-11-26 23:11:58 -0500

    Add minor comments.

--- ruky/src/mcts.rs
+++ ruky/src/mcts.rs
@@ -14,6 +14,7 @@ struct Node<'a> {
     is_leaf: bool,
 }
 
+// Creates a Node from a reference to a Board.
 impl From<&Board> for Node<'_> {
     fn from(board: &Board) -> Self {
         Self {
@@ -30,12 +31,14 @@ impl From<&Board> for Node<'_> {
 }
 
 impl Node<'_> {
+    // Creates a Node from a board and a prior.
     fn with_prior(board: &Board, prior: f32) -> Self {
         let mut node = Node::from(board);
         node.prior = prior;
         node
     }
 
+    // Creates a Node from a board, a parent, and a prior.
     fn with_parent_and_prior<'a>(board: &Board, parent: &'a Node, prior: f32) -> Node<'a> {
         let mut node = Node::from(board);
         node.parent = Some(parent);

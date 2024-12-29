9038dd224f02d68e6d21cbc24422e7d902f427c5
Omar <oser520@gmail.com>
2024-12-28 22:56:07 -0500

    Impl Game to be able to play a self play game.
    This change includes the following:
    * adds member board to struct Game to be the starting position.
    * renames some fields
    * creates GameWinner enum to represent the game winner
    * create strut GameResult to return the SearchResult of every move, the
      game winner, and the starting position.

--- ruky/src/game.rs
+++ ruky/src/game.rs
@@ -1,21 +1,74 @@
 // This module contains components to play games of chess.
 
-use crate::search::Search;
+use crate::board::{Board, GameState};
+use crate::err::RukyErr;
+use crate::piece::Color;
+use crate::search::{Search, SearchResult};
 use std::rc::Rc;
 
 // A struct to represent a game between two players.
 pub struct Game<S: Search> {
-    white_player: Rc<S>,
-    black_player: Rc<S>,
+    board: Board,
+    white_search: Rc<S>,
+    black_search: Rc<S>,
     max_moves: u32,
 }
 
 impl<S: Search> Game<S> {
-    pub fn create(white_player: Rc<S>, black_player: Rc<S>, max_moves: u32) -> Self {
+    pub fn create(board: Board, white_search: Rc<S>, black_search: Rc<S>, max_moves: u32) -> Self {
         Self {
-            white_player,
-            black_player,
+            board,
+            white_search,
+            black_search,
             max_moves,
+        }
+    }
+
+    pub fn play(&self) -> Result<GameResult, RukyErr> {
+        let mut moves = Vec::<SearchResult>::new();
+        let mut next_board = &self.board;
+        for _ in 0..self.max_moves {
+            let result = match next_board.is_white_next() {
+                true => self.white_search.search_board(next_board)?,
+                false => self.black_search.search_board(next_board)?,
+            };
+            moves.push(result);
+            next_board = moves.last().unwrap().best_board();
+            if next_board.is_terminal() {
+                break;
+            }
+        }
+        let winner = GameWinner::from(moves.last().unwrap().best_board().game_state());
+        Ok(GameResult {
+            board: self.board.clone(),
+            moves,
+            winner,
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct GameResult {
+    board: Board,
+    moves: Vec<SearchResult>,
+    winner: GameWinner,
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+pub enum GameWinner {
+    Black,
+    White,
+    Draw,
+}
+
+impl From<GameState> for GameWinner {
+    fn from(game_state: GameState) -> Self {
+        match game_state {
+            GameState::Mate(color) => match color {
+                Color::White => GameWinner::Black,
+                Color::Black => GameWinner::White,
+            },
+            _ => GameWinner::Draw,
         }
     }
 }

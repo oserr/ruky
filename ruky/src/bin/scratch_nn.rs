9ed0cfaa013e630d3144c94452bf7e86e3d00c53
Omar <oser520@gmail.com>
2024-12-31 00:00:44 -0500

    Add stuff to debug decoding, tensor values, etc.

--- ruky/src/bin/scratch_nn.rs
+++ ruky/src/bin/scratch_nn.rs
@@ -4,6 +4,9 @@
 use burn::backend::candle::{Candle, CandleDevice};
 use burn::tensor::Tensor;
 use ruky::nn::AlphaZeroNet;
+use ruky::tensor_decoder::{AzDecoder, TensorDecoder};
+use ruky::tensor_encoder::{AzEncoder, TensorEncoder};
+use ruky::Ruky;
 
 fn main() {
     let device = CandleDevice::cuda(0);
@@ -12,4 +15,24 @@ fn main() {
     let (pol, val) = alpha_zero.forward(t);
     println!("pol={}", pol);
     println!("val={}", val);
+
+    let ruky = Ruky::new();
+    let board = ruky.new_board();
+    let enc = AzEncoder::new(device.clone());
+    let t = enc.encode_board(&board);
+    println!("t={}", t);
+    let (pol, val) = alpha_zero.forward(t);
+    println!("pol={}", pol);
+    println!("val={}", val);
+
+    let dec = AzDecoder::<Candle>::new();
+    let dec_boards = dec
+        .decode_boards(&board, pol, val)
+        .expect("Expecting decdoed boards.");
+    println!("Decoded tensor:-----");
+    println!("value={}", dec_boards.value);
+    for (board, prior) in dec_boards.board_probs {
+        let last_move = board.last_move().expect("Expecting a last move.");
+        println!("\tprior={} move={:?}", prior, last_move);
+    }
 }

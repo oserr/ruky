5b2950afabccf150196b7ad6d93b04c83ad3656a
Omar <oser520@gmail.com>
2024-12-30 23:59:36 -0500

    Fix issues with encoding move.

--- ruky/src/ecmv.rs
+++ ruky/src/ecmv.rs
@@ -2,6 +2,7 @@
 
 use crate::piece::Piece;
 use crate::piece_move::PieceMove;
+use std::cmp::max;
 
 // Represents the code for a given piece move, i.e, Piece<PieceMove>.
 pub(crate) struct EcMove {
@@ -16,9 +17,9 @@ impl EcMove {
         self.row * 8 + self.col
     }
 
-    // Maps EcMove to a number in [0, code * 64 + row * 8 + col).
+    // Maps EcMove to a number in [0, 8x8x73).
     pub fn index(&self) -> usize {
-        self.code as usize * 64 + (self.row * 8 + self.col) as usize
+        (self.row as usize + 1) * (self.col as usize + 1) * self.code as usize - 1
     }
 }
 
@@ -49,21 +50,21 @@ impl From<Piece<PieceMove>> for EcMove {
     }
 }
 
-// Converts a knight move to a number in the range [0, 7].
+// Converts a knight move to a number in the range [1, 8].
 fn encode_knight_move(row_diff: i8, col_diff: i8) -> u8 {
     match (row_diff, col_diff) {
         // Lower left quadrant.
-        (-2, -1) => 0,
-        (-1, -2) => 1,
+        (-2, -1) => 1,
+        (-1, -2) => 2,
         // Upper left quadrant.
-        (1, -2) => 2,
-        (2, -1) => 3,
+        (1, -2) => 3,
+        (2, -1) => 4,
         // Upper right quadrant.
-        (2, 1) => 4,
-        (1, 2) => 5,
+        (2, 1) => 5,
+        (1, 2) => 6,
         // Lower right quadrant.
-        (-1, 2) => 6,
-        (-2, 1) => 7,
+        (-1, 2) => 7,
+        (-2, 1) => 8,
         _ => panic!(
             "row_diff={} and col_diff={} don't make sense for a knight move.",
             row_diff, col_diff
@@ -72,14 +73,14 @@ fn encode_knight_move(row_diff: i8, col_diff: i8) -> u8 {
 }
 
 // Converts an underpromotion move, i.e. a promotion to a piece other than a
-// queen, to a number in the range [0, 8].
+// queen, to a number in the range [1, 9].
 fn encode_under_promo(col_diff: i8, promo: Piece<()>) -> u8 {
     assert!(col_diff >= -1 && col_diff <= 1);
 
     let code = match promo {
-        Piece::Rook(_) => 0,
-        Piece::Bishop(_) => 3,
-        Piece::Knight(_) => 6,
+        Piece::Rook(_) => 1,
+        Piece::Bishop(_) => 4,
+        Piece::Knight(_) => 7,
         _ => panic!("piece={:?} is not valid for under promotion.", promo),
     };
 
@@ -90,32 +91,69 @@ fn encode_under_promo(col_diff: i8, promo: Piece<()>) -> u8 {
     }
 }
 
-// Converts a queen move to a number in the range [0, 55].
+// Converts a queen move to a number in the range [1, 56].
 fn encode_queen_move(row_diff: i8, col_diff: i8) -> u8 {
-    assert!(row_diff >= -7 && row_diff <= 7);
-    assert!(col_diff >= -7 && col_diff <= 7);
-    assert!(row_diff != 0 || col_diff != 0);
+    let direction = Direction::from_row_col_diff(row_diff, col_diff).to_u8();
+    let num_sq = max(row_diff.unsigned_abs(), col_diff.unsigned_abs());
+    direction * num_sq
+}
 
-    if row_diff < 0 {
-        let nrows = (row_diff * -1) as u8;
-        if col_diff == 0 {
-            nrows
-        } else if col_diff < 0 {
-            nrows + 7
+// An enum to encode the direction in which the queen moves.
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+enum Direction {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
+}
+
+impl Direction {
+    // Creates the direction from a row_diff and col_diff, where the diffs represent
+    // the difference between the destination and source squares.
+    fn from_row_col_diff(row_diff: i8, col_diff: i8) -> Self {
+        assert!(row_diff >= -7 && row_diff <= 7);
+        assert!(col_diff >= -7 && col_diff <= 7);
+        assert!(row_diff != 0 || col_diff != 0);
+
+        if row_diff == 0 {
+            if col_diff > 0 {
+                Direction::East
+            } else {
+                Direction::West
+            }
+        } else if row_diff > 0 {
+            if col_diff == 0 {
+                Direction::North
+            } else if col_diff > 0 {
+                Direction::NorthEast
+            } else {
+                Direction::NorthWest
+            }
         } else {
-            nrows + 14
+            if col_diff == 0 {
+                Direction::South
+            } else if col_diff > 0 {
+                Direction::SouthEast
+            } else {
+                Direction::SouthWest
+            }
         }
-    } else if row_diff == 0 {
-        if col_diff < 0 {
-            21 + (col_diff * -1) as u8
-        } else {
-            28 + col_diff as u8
+    }
+
+    fn to_u8(&self) -> u8 {
+        match *self {
+            Direction::North => 1,
+            Direction::NorthEast => 2,
+            Direction::East => 3,
+            Direction::SouthEast => 4,
+            Direction::South => 5,
+            Direction::West => 6,
+            Direction::SouthWest => 7,
+            Direction::NorthWest => 8,
         }
-    } else if col_diff == 0 {
-        35 + row_diff as u8
-    } else if col_diff < 0 {
-        42 + row_diff as u8
-    } else {
-        49 + row_diff as u8
     }
 }

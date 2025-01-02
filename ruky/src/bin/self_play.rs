8a18bf62d99c4c3937f8e8abe496ec49bb352c90
Omar <oser520@gmail.com>
2025-01-01 23:43:02 -0500

    Print timing info for each move.

--- ruky/src/bin/self_play.rs
+++ ruky/src/bin/self_play.rs
@@ -16,21 +16,39 @@ fn main() {
         .build()
         .expect("Expecting a new game.");
     println!("Starting a game of self play...");
+    let verbose = false;
     let now = Instant::now();
     let result = game.play().expect("Expecting game result.");
     let dur = now.elapsed();
     let mut color = result.board.color();
     for (i, search_result) in result.moves.iter().enumerate() {
+        let eval_time_per_expansion = search_result.eval_time_per_expansion();
+        let search_time_per_expansion = search_result.search_time_per_expansion();
         println!(
-            "i={} color={:?} prior={} value={} move={:?}",
+            "i={} color={:?} prior={} value={} move={:?}
+            \tnodes_expanded={} nodes_visited={} depth={}
+            \teval_time_per_expansion_millis={} search_time_per_expansion_millis={}
+            \ttotal_eval_time: mins={} secs={}
+            \ttotal_search_time: mins={} secs={}",
             i,
             color,
             search_result.best.prior,
             search_result.value,
-            search_result.best_move()
+            search_result.best_move(),
+            search_result.nodes_expanded,
+            search_result.nodes_visited,
+            search_result.depth,
+            eval_time_per_expansion.as_millis(),
+            search_time_per_expansion.as_millis(),
+            as_mins(&search_result.total_eval_time),
+            search_result.total_eval_time.as_secs_f32(),
+            as_mins(&search_result.total_search_time),
+            search_result.total_search_time.as_secs_f32(),
         );
-        for mp in &search_result.moves {
-            println!("\tprior={} visits={} move={:?}", mp.prior, mp.visits, mp.pm);
+        if verbose {
+            for mp in &search_result.moves {
+                println!("\tprior={} visits={} move={:?}", mp.prior, mp.visits, mp.pm);
+            }
         }
         color = color.flip();
     }

177d5bc7690f28c6e5149146089f7a9e67c3caac
Omar <oser520@gmail.com>
2025-02-09 15:03:21 -0500

    Add other Trainer fields: num_games, check_point_dir, etc.

--- ruky/src/trainer.rs
+++ ruky/src/trainer.rs
@@ -1,8 +1,15 @@
 // This module contains components for a trainer.
 
-use crate::game::TrainingGame;
-use crate::search::{SpSearch, TreeSize};
+use std::path::PathBuf;
 
-pub struct Trainer<S: SpSearch + TreeSize> {
-    game: TrainingGame<S>,
+// The purpose of the Trainer is to play games of self-play to generate training
+// data, and to train the model with the data generated during self-play.
+pub struct Trainer {
+    num_games: usize,
+    // The path to the directory to use for checkpoints.
+    check_point_dir: Option<PathBuf>,
+    // The number of training steps to run before creating a checkpoint.
+    check_point_step: usize,
+    // The batch size to use during training.
+    batch_size: usize,
 }

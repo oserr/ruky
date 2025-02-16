9d5b31f6c40f3174db6d596d91140e9fe3827b7d
Omar <oser520@gmail.com>
2025-02-16 11:23:58 -0500

    Add more Trainer fields.

--- ruky/src/trainer.rs
+++ ruky/src/trainer.rs
@@ -1,10 +1,30 @@
 // This module contains components for a trainer.
 
+use crate::Board;
+use burn::prelude::{Backend, Device};
 use std::path::PathBuf;
 
 // The purpose of the Trainer is to play games of self-play to generate training
 // data, and to train the model with the data generated during self-play.
-pub struct Trainer {
+pub struct Trainer<B: Backend> {
+    // The initial starting position.
+    board: Board,
+    // The device to use for neural network computations.
+    device: Device<B>,
+    // The total number of simulations per move.
+    sims: u32,
+    // The maximum number of moves to play before declaring draw.
+    max_moves: u32,
+    // If true, noise is added to the root node priors.
+    use_noise: bool,
+    // If true, the best move is selected by sampling, otherwise visit count
+    // is used to select the best move.
+    sample_action: bool,
+    // The batch size to use during inference.
+    inference_batch_size: usize,
+    // The number of worker threads to use in search.
+    num_workers: usize,
+    // The number of games to play for training.
     num_games: usize,
     // The path to the directory to use for checkpoints.
     check_point_dir: Option<PathBuf>,

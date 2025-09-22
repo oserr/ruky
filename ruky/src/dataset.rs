ffb74b5ab0f06fde73bf1af22b6edcd9ff5aad44
Omar <oser520@gmail.com>
2025-09-21 21:50:02 -0400

    Create struct GamesBatch to represent a batch of games.

--- ruky/src/dataset.rs
+++ ruky/src/dataset.rs
@@ -1,12 +1,15 @@
 use crate::game::GameResult;
-use burn::data::dataset::Dataset;
+use burn::{
+    data::dataset::Dataset,
+    prelude::{Backend, Tensor},
+};
 
 #[derive(Clone)]
-struct GameDataset {
+pub(crate) struct GamesDataset {
     dataset: Vec<GameResult>,
 }
 
-impl Dataset<GameResult> for GameDataset {
+impl Dataset<GameResult> for GamesDataset {
     fn get(&self, index: usize) -> Option<GameResult> {
         self.dataset.get(index).cloned()
     }
@@ -14,4 +17,14 @@ impl Dataset<GameResult> for GameDataset {
     fn len(&self) -> usize {
         self.dataset.len()
     }
+}
+
+#[derive(Clone, Debug)]
+pub struct GamesBatch<B: Backend> {
+    // A single input represents a game position.
+    pub inputs: Tensor<B, 4>,
+
+    // A single target represents two outputs: 1) the tensor of probabilities
+    // for moves and 2) the value of a given of the given position.
+    pub targets: (Tensor<B, 4>, Tensor<B, 2>),
 }

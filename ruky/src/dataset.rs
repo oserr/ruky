376095e2cc15709b08c431d7706cc3ff43853b14
Omar <oser520@gmail.com>
2025-09-21 21:34:21 -0400

    Impl Dataset<GameResult> for GameDataset.

--- ruky/src/dataset.rs
+++ ruky/src/dataset.rs
@@ -1,6 +1,17 @@
 use crate::game::GameResult;
+use burn::data::dataset::Dataset;
 
 #[derive(Clone)]
 struct GameDataset {
-    game_results: Vec<GameResult>,
+    dataset: Vec<GameResult>,
+}
+
+impl Dataset<GameResult> for GameDataset {
+    fn get(&self, index: usize) -> Option<GameResult> {
+        self.dataset.get(index).cloned()
+    }
+
+    fn len(&self) -> usize {
+        self.dataset.len()
+    }
 }

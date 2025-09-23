80ac9c3103dbe7612aa8b6e1c2cc02934e309f95
Omar <oser520@gmail.com>
2025-09-22 23:19:14 -0400

    Create struct GamePosition.
    The GameDataset will consist of GamePositions, which will be used to
    create the inputs and targets used in training.

--- ruky/src/dataset.rs
+++ ruky/src/dataset.rs
@@ -1,8 +1,25 @@
-use crate::game::GameResult;
+use crate::board::Board;
+use crate::game::{GameResult, GameWinner};
+use crate::search::Mp;
 use burn::{
     data::{dataloader::batcher::Batcher, dataset::Dataset},
     prelude::{Backend, Tensor},
 };
+
+// The game position is a struct with all the information needed to construct
+// the inputs and targets used to train the neural network.
+pub(crate) struct GamePosition {
+    // The board represents the board position, including board state, e.g.
+    // color to move next.
+    pub board: Board,
+    // If game winner is White, and player to move is black, then the value for
+    // the current move is -1. If white is next to move, then value is 1, and if
+    // this is a draw then target valulue is 0.
+    pub game_winner: GameWinner,
+    // The legal moves in the current positions, with visit counts. These are
+    // used to create the target policy tensors.
+    pub moves: Vec<Mp>,
+}
 
 #[derive(Clone)]
 pub(crate) struct GamesDataset {

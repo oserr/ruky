82b5fa430e8bfd18be18bdba57e2f6570d89a2fc
Omar <oser520@gmail.com>
2025-10-04 13:03:07 -0400

    Create training binary.
    The binary will be used to train the network guiding the MCTS.

---
+++ ruky/src/bin/training.rs
@@ -0,0 +1,61 @@
+use burn::backend::cuda::{Cuda, CudaDevice};
+use clap::Parser;
+use ruky::trainer::TrainerBuilder;
+use ruky::Ruky;
+use std::time::{Duration, Instant};
+
+fn main() {
+    let args = Args::parse();
+    let ruky = Ruky::new();
+    let trainer = TrainerBuilder::<Cuda>::new()
+        .device(CudaDevice::new(0))
+        .board(ruky.new_board())
+        .num_games(args.games)
+        .num_sessions(args.sessions)
+        .check_point_dir(&args.out_dir)
+        .training_batch_size(args.training_batch_size)
+        .training_percent(args.training_percent)
+        .build()
+        .expect("Expecting a trainer.");
+    println!("Running the training pipeline...");
+    let now = Instant::now();
+    if let Err(_) = trainer.run_training() {
+        eprintln!("Error running running training pipeline.");
+    } else {
+        let mins = as_mins(&now.elapsed());
+        println!("Finished training in {} minutes.", mins);
+    }
+}
+
+fn as_mins(dur: &Duration) -> f32 {
+    dur.as_secs_f32() / 60.0
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The number of games to self-play for one session. These games are
+    /// subsequently used in a round of training.
+    #[arg(short, long)]
+    games: usize,
+
+    /// The number of sessions to train for. 1 session includes one round of
+    /// self-play, a round of training, and a tournament match between the old
+    /// and newly trained model.
+    #[arg(short, long)]
+    sessions: usize,
+
+    /// The output directory where training artifacts are saved, which may
+    /// include games of self-play, training check-points, and models.
+    #[arg(short, long)]
+    out_dir: String,
+
+    /// The training batch size.
+    #[arg(short, long, default_value_t = 32)]
+    training_batch_size: usize,
+
+    /// The percent of games that are used for training data. The difference are
+    /// used as a validation set.
+    #[arg(short, long, default_value_t = 0.9)]
+    training_percent: f32,
+}

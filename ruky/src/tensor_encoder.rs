37d9072e493e0e67f78f86110c851535f6d9c884
Omar <oser520@gmail.com>
2025-10-17 23:22:39 -0400

    Reformat comment for AzEncoder.

--- ruky/src/tensor_encoder.rs
+++ ruky/src/tensor_encoder.rs
@@ -101,14 +101,14 @@ pub trait TensorEncoder<B: Backend> {
     fn encode_batch_data(&self, batch_size: usize, data: Vec<f32>) -> Tensor<B, 4>;
 }
 
-// AzEncoder represents the AlphaZero encoder, i.e. it encodes the board the
-// board state for input into the AlphaZero network. For a given board position,
-// it outputs a 119 x 8 x 8 tensor. 6 planes are used to represent one set of
-// the pieces, 6 for the other set of pieces, and 2 planes for repetition count
-// in current position for each player. Hence, the current board position uses
-// 14 total planes, but this is repeated for 8 total time steps. If there are no
-// previous positions, the planes are set to zeros. The last 7 planes are used
-// to represent
+// AzEncoder represents the AlphaZero encoder, i.e. it encodes the board state
+// for input into the AlphaZero network. For a given board position, it outputs
+// a 119 x 8 x 8 tensor. 6 planes are used to represent one set of the pieces, 6
+// for the other set of pieces, and 2 planes for repetition count in current
+// position for each player. Hence, the current board position uses 14 total
+// planes, but this is repeated for 8 total time steps. If there are no previous
+// positions, the planes are set to zeros. The last 7 planes are used to
+// represent
 // - 1 for the current color
 // - 1 for the total move count
 // - 1 for king castling for the current player

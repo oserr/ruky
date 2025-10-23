672c913e3744a253fc167efec74c87958115c5b3
Omar <oser520@gmail.com>
2025-10-22 23:17:47 -0400

    Replace candle backend with Cuda backend.
    The Candle backend depends on an old version of Cudarc that is not
    compatible with the latest version of Cuda (i.e. 13).

--- ruky/src/bin/scratch_nn.rs
+++ ruky/src/bin/scratch_nn.rs
@@ -1,20 +1,20 @@
 // This is simply a scratch pad to combine and tests stuff, either to verify
 // that it works, or simply to figure out how something works.
 
-use burn::backend::candle::{Candle, CandleDevice};
+use burn::backend::cuda::{Cuda, CudaDevice};
 use burn::module::Module;
 use burn::record::{FullPrecisionSettings, NamedMpkFileRecorder};
 use ruky::nn::AlphaZeroNet;
 
 fn main() {
-    let device = CandleDevice::cuda(0);
-    let alpha_zero = AlphaZeroNet::<Candle>::new(&device);
+    let device = CudaDevice::cuda(0);
+    let alpha_zero = AlphaZeroNet::<Cuda>::new(&device);
     let recorder = NamedMpkFileRecorder::<FullPrecisionSettings>::new();
     alpha_zero
         .save_file("/home/omar/burn_model", &recorder)
         .expect("Alpha zero model is saved.");
 
-    let other_net = AlphaZeroNet::<Candle>::new(&device);
+    let other_net = AlphaZeroNet::<Cuda>::new(&device);
     other_net
         .load_file("/home/omar/burn_model", &recorder, &device)
         .expect("Alpha zero model is saved.");
